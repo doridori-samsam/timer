bf8fe01b21d61db19d2df79e784e0187ecd07ef9
Sam <wtawonlife@gmail.com>
2022-07-03 19:22:03 +0900

    timer function (only for minutes and seconds) updated

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* 타임 셋 */
-
 const timeBox = document.querySelectorAll('.box');
-
 
 for(let i=0; i<timeBox.length; i++){
   timeBox[i].addEventListener('click', (e)=>{
@@ -25,11 +23,15 @@ for(let i=0; i<timeBox.length; i++){
     resetBtn.disabled = false;
     resetBtn.classList.add('abled');
   })
+
 };
 
 
 /* 타이머 구동 */
+let isOn = false;
+
 function countDown() {
+  if(isOn){
   for(let i=0; i<timeBox.length-2; i++){
     console.log(timeBox[2].textContent);
     if(timeBox[2].textContent > 0){
@@ -40,11 +42,24 @@ function countDown() {
       else{timeBox[2].textContent = parseInt(timeBox[2].textContent)-1;
       }
     }
+    else if(timeBox[2].textContent === '00'){
+      if(parseInt(timeBox[1].textContent) === 0){
+        isOn = false;
+        clearInterval(interval);
+        console.log(isOn);
+      }
+      else if(timeBox[1].textContent <= 10){
+        timeBox[1].textContent = '0' + parseInt(parseInt(timeBox[1].textContent)-1);}
+        else if(timeBox[1].textContent > 10){
+          timeBox[1].textContent = parseInt(parseInt(timeBox[1].textContent)-1);}
+      
+          timeBox[2].textContent = 59;
+    }
+  }
   }
 };
 
-
-
+let interval;
 
 
 /* 버튼 클릭 이벤트 */
@@ -53,26 +68,41 @@ const resetBtn = document.querySelector('.reset');
 const pauseBtn = document.getElementsByClassName('.pause');
 
 resetBtn.addEventListener('click', ()=>{
-  for(let nums of timeBox){
-    nums.textContent = '00';
-    resetBtn.disabled = true;
-    resetBtn.classList.remove('abled');
-    startBtn.disabled = true;
-    startBtn.classList.remove('abled');
+  if(resetBtn.classList[0] === 'reset'){
+    for(let nums of timeBox){
+      nums.textContent = '00';
+    }
+      resetBtn.disabled = true;
+      resetBtn.classList.remove('abled');
+      startBtn.disabled = true;
+      startBtn.classList.remove('abled');
+  } else if(resetBtn.classList[0] === 'pause'){
+      clearInterval(interval);
+      isOn = null;
+      console.log(isOn);
+      startBtn.disabled = false;
+      startBtn.classList.add('abled');
+      resetBtn.classList.remove('pause')
+      resetBtn.classList.add('reset');
+      resetBtn.classList.add('abled');
   }
-  console.log('리셋!');
 });
 
 
-
 startBtn.addEventListener('click', ()=>{
-  resetBtn.classList.add('pause');
+  if(isOn === null){
+    isOn = true;
+    console.log(isOn);
+    interval = setInterval(countDown, 300);
+  }
+  isOn = true;
+  interval = setInterval(countDown, 300);
   resetBtn.classList.remove('reset');
+  resetBtn.classList.add('pause');
+  resetBtn.classList.remove('abled');
   startBtn.classList.remove('abled');
   startBtn.disabled = true;
   console.log('시작!');
 })
 
-pauseBtn.addEventListener('click', ()=>{
-  console.log('ㅠㅠ');
-})
+
